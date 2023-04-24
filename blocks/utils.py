64533112f6f2e4d05857bfe12170d2091c873576
Pupy101 <ser.porkhun41@gmail.com>
2023-04-24 07:04:27 +0300

    Add new utils functions

--- blocks/utils.py
+++ blocks/utils.py
@@ -1,5 +1,27 @@
+import random
+from typing import Optional
+
+import numpy as np
+import torch
 from torch import nn
 
 
-def count_parameters(model: nn.Module) -> int:
-    return sum(_.numel() for _ in model.parameters())
+def count_parameters(model: nn.Module, requires_grad: Optional[bool] = None) -> int:
+    count = 0
+    for params in model.parameters():
+        if requires_grad is not None and params.requires_grad != requires_grad:
+            continue
+        count += params.numel()
+    return count
+
+
+def freeze_weight(model: nn.Module) -> None:
+    for weight in model.parameters():
+        weight.requires_grad = False
+
+
+def set_seed(seed: int) -> None:
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
