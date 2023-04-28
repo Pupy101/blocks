090d014f96a60bdd387a4f244e692d3930baed48
Pupy101 <ser.porkhun41@gmail.com>
2023-04-28 22:29:39 +0300

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def find_requirements() -> List[str]:
 
 setup(
     name="blocks",
-    version="0.2",
+    version="0.1.1",
     description="Library with torch blocks",
     author="Sergei Porkhun",
     author_email="ser.porkhun41@gmail.com",
